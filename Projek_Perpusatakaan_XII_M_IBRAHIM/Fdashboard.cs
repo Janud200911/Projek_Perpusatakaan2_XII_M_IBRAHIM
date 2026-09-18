0777aed0638bb883b890dc9cda0a15d6fd0a6d98
Janud200911 <mibrahimalhabsi907@gmail.com>
2026-09-18 11:14:32 +0700

    Coba

---
+++ Projek_Perpusatakaan_XII_M_IBRAHIM/Fdashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projek_Perpusatakaan_XII_M_IBRAHIM
+{
+    public partial class Fdashboard : Form
+    {
+        public Fdashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
